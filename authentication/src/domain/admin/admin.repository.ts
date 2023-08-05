511f9aa656bcc51c8f4a8fa358a9bb55147b9688
iamrosada <luisrosada2018@gmail.com>
2023-08-05 15:57:49 +0300

    feat: added forgotPassword

--- authentication/src/domain/admin/admin.repository.ts
+++ authentication/src/domain/admin/admin.repository.ts
@@ -1,7 +1,8 @@
-import { AdminEntity } from "./admin.entity";
+import { AdminEntity, ForgotPasswordResponse } from "./admin.entity";
 
 export interface AdminRepository {
   findAdminById(uuid: string): Promise<AdminEntity | null>;
   registerAdmin(Admin: AdminEntity): Promise<AdminEntity | null>;
   listAdmin(): Promise<AdminEntity[] | null>;
+  forgotPassword(uuid: string): Promise<ForgotPasswordResponse>;
 }
