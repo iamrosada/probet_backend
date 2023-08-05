8484f8dc2ec194c40101976150e95dfec9ea8992
iamrosada <luisrosada2018@gmail.com>
2023-08-05 15:37:43 +0300

    feat: change adminEntity added some variable that will be necessary to create a strong profile to our admin

--- authentication/src/domain/admin/admin.entity.ts
+++ authentication/src/domain/admin/admin.entity.ts
@@ -1,5 +1,12 @@
 export interface AdminEntity {
   uuid: string;
+  firstName: string;
+  lastName: string;
   email: string;
   password: string;
+  accessToken?: string;
+  forgotPasswordAccessToken?: number;
+  forgotPasswordExpiresIn?: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
