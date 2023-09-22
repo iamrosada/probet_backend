d174a6c5c3b5d833f40ec7d47d25a750df1efeda
iamrosada <luisrosada2018@gmail.com>
2023-09-22 11:51:12 +0300

    feat: added interface from global types

---
+++ apps/user-service/src/infra/controller/admin.ctrl.ts
@@ -0,0 +1,40 @@
+import { AdminUseCase } from "../../application/usecases/create-admin";
+import { AuthAdminToDashboardUseCase } from "../../application/usecases/auth-admin-to-dashboard";
+import { AdminEntity } from "../../domain/admin-entity/admin";
+
+export class AdminController {
+  constructor(private adminUseCase: AdminUseCase, private authAdminToDashboardUseCase: AuthAdminToDashboardUseCase) {
+    this.insertCtrl = this.insertCtrl.bind(this);
+    this.authCtrl = this.authCtrl.bind(this);
+  }
+
+  public async insertCtrl(input: Input) {
+    const admin = await this.adminUseCase.registerAdmin(
+      input.password,
+      input.email,
+      input.numberPhone,
+      input.createdAt,
+      input.updatedAt,
+    );
+
+    return admin;
+  }
+
+
+  public async authCtrl(input: InputAdminAuth) {
+    const admin = await this.authAdminToDashboardUseCase.authentication(
+      input
+    );
+
+    return admin;
+  }
+}
+
+type Input = {
+  password: string,
+  email: string,
+  numberPhone: string,
+  createdAt: Date,
+  updatedAt: Date
+};
+
