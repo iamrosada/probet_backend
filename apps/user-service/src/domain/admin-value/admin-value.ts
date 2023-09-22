cdeaac4e5a27c13cee964bb2b1955e4146ab464c
iamrosada <luisrosada2018@gmail.com>
2023-09-22 11:21:25 +0300

    feat: created the AdminValue

---
+++ apps/user-service/src/domain/admin-value/admin-value.ts
@@ -0,0 +1,30 @@
+import { AdminEntity } from "../admin-entity/admin";
+
+
+
+export class AdminValue implements AdminEntity {
+  uuid: string;
+  email: string;
+  password: string;
+  numberPhone: string;
+  createdAt?: Date | undefined;
+  updatedAt?: Date | undefined;
+
+  constructor({
+    password,
+    email,
+    numberPhone,
+    createdAt,
+    updatedAt,
+  }: Omit<
+    AdminEntity,
+    "uuid"
+  >) {
+    this.uuid = uuid();
+    this.password = password;
+    this.email = email;
+    this.numberPhone = numberPhone;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
