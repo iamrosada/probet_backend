a874808dc4e6308427c16472aecfb25c7768c2bf
iamrosada <luisrosada2018@gmail.com>
2023-09-22 11:37:53 +0300

    feat: created the admin core

---
+++ apps/user-service/src/infra/core/admin-core.ts
@@ -0,0 +1,22 @@
+import { AuthAdminToDashboardUseCase } from "../../application/usecases/auth-admin-to-dashboard";
+import { AdminUseCase } from "../../application/usecases/create-admin";
+import { AdminController } from "../controller/admin.ctrl";
+import { PrismaAdminRepository } from "../repositories/prisma-admins-repository";
+
+/**
+ * Iniciar Repository
+ */
+const adminRepo = new PrismaAdminRepository();
+
+/**
+ * Iniciamos casos de uso
+ */
+
+const adminUseCase = new AdminUseCase(adminRepo);
+const adminAuthUseCase = new AuthAdminToDashboardUseCase(adminRepo)
+
+/**
+ * Iniciar admin Controller
+ */
+
+export const adminCtrl = new AdminController(adminUseCase, adminAuthUseCase);
