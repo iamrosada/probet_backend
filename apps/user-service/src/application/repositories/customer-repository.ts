4ea5c0016212ea6d7dffed999c75f7b9b0dcd410
iamrosada <luisrosada2018@gmail.com>
2023-09-22 10:57:59 +0300

    feat: updated customer repository

--- apps/user-service/src/application/repositories/customer-repository.ts
+++ apps/user-service/src/application/repositories/customer-repository.ts
@@ -1,14 +1,16 @@
 import {
-  CustomerEntity, CustomerEntitySpeed,
+  CustomerEntity,
 } from '../../domain/customer-entity/customer';
 
 export interface CustomerRepository {
-  authExpireIn24(customer: InputExpireIn24Speed): Promise<string>
-  auth(customer: InputCustomer): Promise<string>
-  create(customer: CustomerEntity): Promise<void>;
-  createCustomerFor24h(customer: CustomerEntitySpeed): Promise<void>
+  authExpireIn24(customer: InputExpireIn24SpeedAuth): Promise<OutPutCustomerAuth>
+  auth(customer: InputCustomerAuth): Promise<OutPutCustomerAuth>
+  create(customer: Omit<CustomerEntity, 'uuid'>): Promise<void>;
+  createCustomerFor24h(customer: InputCreateSpeed): Promise<void>
   findByNumberPhone(phone: string): Promise<CustomerEntity | null>
-  list(customers: CustomerEntity): Promise<CustomerEntity[] | void>
+  list(): Promise<CustomerEntity[] | null>
+  findCustomerById(id: string): Promise<CustomerEntity | null>
+  findCustomerByDateCreation(createdAt: Date): Promise<CustomerEntity | null>
 }
 
 
@@ -16,20 +18,4 @@ export interface CustomerRepository {
 
 
 
-type InputCustomer = {
-  uuid: string;
-  numberPhone: string;
-  password: string;
-  accessToken?: string;
-  forgotPasswordAccessToken?: number;
-  forgotPasswordExpiresIn?: Date;
-}
-
-type InputExpireIn24Speed = {
-  uuid: string;
-  numberPhone: string;
-  accessToken?: string;
-  forgotPasswordAccessToken?: number;
-  forgotPasswordExpiresIn?: Date;
-}
 
