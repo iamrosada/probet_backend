14d7bc44fb656abf2b05f2a501af0abe1d969bfb
iamrosada <luisrosada2018@gmail.com>
2023-09-28 21:00:57 +0300

    fix: removed void on return and added the Entity Customer when is create the new customer

--- apps/user-service/src/application/repositories/customer-repository.ts
+++ apps/user-service/src/application/repositories/customer-repository.ts
@@ -5,7 +5,7 @@ import {
 export interface CustomerRepository {
   authExpireIn24(customer: InputExpireIn24SpeedAuth): Promise<OutPutCustomerAuth>
   auth(customer: InputCustomerAuth): Promise<OutPutCustomerAuth>
-  create(customer: Omit<CustomerEntity, 'uuid'>): Promise<void>;
+  create(customer: Omit<CustomerEntity, 'uuid'>): Promise<CustomerEntity>;
   createCustomerFor24h(customer: InputCreateSpeed): Promise<void>
   findByNumberPhone(phone: string): Promise<CustomerEntity | null>
   list(): Promise<CustomerEntity[] | null>
@@ -14,7 +14,29 @@ export interface CustomerRepository {
 }
 
 
+type InputCustomerAuth = {
+  numberPhone?: string;
+  password?: string;
+}
+type InputAdminAuth = {
+  email?: string;
+  password?: string;
+}
 
+type OutPutCustomerAuth = {
+  accessToken?: string;
+}
+
+type InputExpireIn24SpeedAuth = {
+  numberPhone: string;
+}
+
+
+type InputCreateSpeed = InputExpireIn24SpeedAuth
+
+type InputExpireIn24Speed = {
+  numberPhone: string;
+}
 
 
 
