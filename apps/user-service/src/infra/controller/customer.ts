a2816612c3301d104cc7573bd0e4855e03141602
iamrosada <luisrosada2018@gmail.com>
2023-09-28 20:23:44 +0300

    fix: fixing the bug when is creating a new customer

--- apps/user-service/src/infra/controller/customer.ts
+++ apps/user-service/src/infra/controller/customer.ts
@@ -1,67 +1,104 @@
-import { Request, Response } from "express";
-import { CustomerUseCase } from "../../application/usecases/create-customer";
+import { CustomerUseCase } from '../../application/usecases/create-customer';
 
 export class CustomerController {
   constructor(private customerUseCase: CustomerUseCase) {
     this.insertCtrl = this.insertCtrl.bind(this);
+    this.FindCustomerByPhoneNumberCtrl = this.FindCustomerByPhoneNumberCtrl.bind(this)
+    // this.authCtrl = this.authCtrl.bind(this);
+    // this.authExpireIn24Ctrl = this.authExpireIn24Ctrl.bind(this);
+    // this.FindByDateCtrl = this.FindByDateCtrl.bind(this);
+    // this.FindByIdCtrl = this.FindByIdCtrl.bind(this);
+    // this.FindAllCustomerCtrl = this.FindAllCustomerCtrl.bind(this);
   }
 
   public async insertCtrl(input: Input) {
-    const customer = await this.customerUseCase.registerCustomer(
-      input.password,
-      input.firstName,
-      input.lastName,
-      input.numberPhone,
-      input.createdAt,
-      input.updatedAt,
-    );
+    //console.log(input)
+    const customerAlreadyExist = await this.FindCustomerByPhoneNumberCtrl(input.numberPhone)
+    console.log(customerAlreadyExist, 'fora promises')
+    if (customerAlreadyExist) {
+      //console.log((customerAlreadyExist), 'like promises')
+      throw new Error('Error Customer Already Exist ');
+    }
+    try {
+      const customer = await this.customerUseCase.registerCustomer(
+        input.password,
+        input.firstName,
+        input.lastName,
+        input.numberPhone,
+      );
 
-    return customer;
+      return customer;
+    } catch (error) {
+      // Trate o erro de forma apropriada, como retornar uma resposta de erro HTTP.
+      throw new Error('Erro ao criar cliente: ' + error.message);
+    }
   }
 
   public async authCtrl(password: string, numberPhone: string) {
-    const customer = await this.customerUseCase.AuthCustomerForLongPeriodBet(
-      {
+    try {
+      const customer = await this.customerUseCase.AuthCustomerForLongPeriodBet({
         password,
-        numberPhone
-      }
+        numberPhone,
+      });
 
-    );
-
-    return customer;
+      return customer;
+    } catch (error) {
+      // Trate o erro de forma apropriada, como retornar uma resposta de erro HTTP.
+      throw new Error('Erro na autenticação: ' + error.message);
+    }
   }
 
   public async authExpireIn24Ctrl(numberPhone: string) {
-    const customer = await this.customerUseCase.AuthCustomerAndExpireAfterBet(
-      {
-        numberPhone
-      }
+    try {
+      const customer = await this.customerUseCase.AuthCustomerAndExpireAfterBet({
+        numberPhone,
+      });
 
-    );
-
-    return customer;
+      return customer;
+    } catch (error) {
+      // Trate o erro de forma apropriada, como retornar uma resposta de erro HTTP.
+      throw new Error('Erro na autenticação com expiração: ' + error.message);
+    }
   }
 
   public async FindByDateCtrl(createdAt: Date) {
-    const customer = await this.customerUseCase.getCustomerByDate(
-      createdAt
-    );
-
-    return customer;
+    try {
+      const customer = await this.customerUseCase.getCustomerByDate(createdAt);
+      return customer;
+    } catch (error) {
+      // Trate o erro de forma apropriada, como retornar uma resposta de erro HTTP.
+      throw new Error('Erro ao buscar cliente por data: ' + error.message);
+    }
   }
 
   public async FindByIdCtrl(uuid: string) {
-    const customer = await this.customerUseCase.getDetailCustomer(
-      uuid
-    );
-
-    return customer;
+    try {
+      const customer = await this.customerUseCase.getDetailCustomer(uuid);
+      return customer;
+    } catch (error) {
+      // Trate o erro de forma apropriada, como retornar uma resposta de erro HTTP.
+      throw new Error('Erro ao buscar cliente por ID: ' + error.message);
+    }
   }
 
   public async FindAllCustomerCtrl() {
-    const customer = await this.customerUseCase.getListOfAllCustomer();
+    try {
+      const customer = await this.customerUseCase.getListOfAllCustomer();
+      return customer;
+    } catch (error) {
+      // Trate o erro de forma apropriada, como retornar uma resposta de erro HTTP.
+      throw new Error('Erro ao buscar todos os clientes: ' + error.message);
+    }
+  }
 
-    return customer;
+  public async FindCustomerByPhoneNumberCtrl(numberPhone: string) {
+    try {
+      const customer = await this.customerUseCase.findByNumberPhone(numberPhone);
+      return customer;
+    } catch (error) {
+      // Trate o erro de forma apropriada, como retornar uma resposta de erro HTTP.
+      throw new Error('Erro ao buscar  cliente por numero de telefone: ' + error.message);
+    }
   }
 }
 
@@ -70,6 +107,4 @@ type Input = {
   firstName: string;
   lastName: string;
   numberPhone: string;
-  createdAt: Date;
-  updatedAt: Date
 };
