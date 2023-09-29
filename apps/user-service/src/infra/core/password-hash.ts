c655abbc66c02e8fe8fd65e49fe2ef033313b499
iamrosada <luisrosada2018@gmail.com>
2023-09-29 12:10:53 +0300

    fix: fixed the error when is created the new password

--- apps/user-service/src/infra/core/password-hash.ts
+++ apps/user-service/src/infra/core/password-hash.ts
@@ -5,35 +5,47 @@ import { promisify } from "util";
 const scryptAsync = promisify(scrypt);
 
 export class Password {
-
   static async hashPassword(password: string) {
-    const salt = randomBytes(16).toString("hex");
+    // Generate a stronger random salt (32 bytes)
+    const salt = randomBytes(32).toString('hex');
 
     try {
-      const buf = (await scryptAsync(password, salt, 64)) as Buffer;
-      return `${buf.toString("hex")}.${salt}`;
+      const buf = await scryptAsync(password, salt, 64) as Buffer;
+      return `${buf.toString('hex')}.${salt}`;
     } catch (err) {
       throw new Error('Error hashing password');
     }
-
   }
 
   static async comparePassword(
     storedPassword: string,
     suppliedPassword: string
   ): Promise<boolean> {
-    // split() returns array
     const [hashedPassword, salt] = storedPassword.split(".");
-    // we need to pass buffer values to timingSafeEqual
-    const hashedPasswordBuf = Buffer.from(hashedPassword, "hex");
-    // we hash the new sign-in password
-    try {
-      const suppliedPasswordBuf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
-      // compare the new supplied password with the stored hashed password
-      return timingSafeEqual(hashedPasswordBuf, suppliedPasswordBuf);
-    } catch (err) {
-      throw new Error('Error comparing passwords');
+
+    if (!hashedPassword || !salt) {
+      return false; // Invalid stored password format
     }
 
+    const hashedPasswordBuf = Buffer.from(hashedPassword, "hex");
+    const suppliedPasswordBuf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    if (!hashedPasswordBuf || !suppliedPasswordBuf) {
+      return false; // Error hashing one of the passwords
+    }
+
+    try {
+      // Compare the new supplied password with the stored hashed password
+      return timingSafeEqual(hashedPasswordBuf, suppliedPasswordBuf);
+    } catch (err) {
+      console.error('Error comparing passwords:', err);
+      return false; // Error comparing passwords
+    }
   }
+
+
+
+
+
+
 }
