d63c1e78b416b1883f67cab0845c289093966d87
iamrosada <luisrosada2018@gmail.com>
2023-09-30 14:18:59 +0300

    feat: created login to customer oob

--- apps/user-service/src/infra/http/index.ts
+++ apps/user-service/src/infra/http/index.ts
@@ -133,6 +133,27 @@ export default function runServer() {
     }
   });
 
+  router.post('/login_customer_oob', async (req: Request, res: Response) => {
+    try {
+      // Validate input data here if needed.
+
+      const user = await customerCtrl.authExpireIn24Ctrl(
+        req.body.numberPhone
+      );
+
+      // return res.status(201).json(user).send();
+      console.log(user)
+      return res.status(200).json({
+        message: "customer authenticated with success",
+        user: user,
+      });
+    } catch (error) {
+      next(error.message); // Pass the error to the error handling middleware
+      res.send(error.message).json();
+      console.log(error.message);
+
+    }
+  });
 
   // Login route
   router.post('/login', async (req: Request, res: Response) => {
