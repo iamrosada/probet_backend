b854f8fce4473bd1820d278fe808d04c99185983
iamrosada <luisrosada2018@gmail.com>
2023-09-30 17:16:39 +0300

    feat: created function to generate token

--- apps/user-service/src/infra/core/auth-global-core.ts
+++ apps/user-service/src/infra/core/auth-global-core.ts
@@ -1,28 +1,49 @@
-import { sign } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
+interface TokenOptions {
+  expiresIn?: string;
+}
+export interface PayloadCustomer {
+  numberPhone: string;
+}
+export class TokenService {
+  JWT_SECRET: string;
+  REFRESH_TOKEN_SECRET: string;
+  constructor() {
+    // Secret key for signing access tokens (use a strong secret)
+    this.JWT_SECRET = "Probet";
 
+    // Secret key for signing refresh tokens (use a strong secret)
+    this.REFRESH_TOKEN_SECRET = "Probet";
+  }
 
-export const createAccessToken = (userId: InputCustomerAuth) => {
-  return sign(
-    {
-      user: userId,
-    },
-    process.env.ACCESS_TOKEN_SECRET!,
-    {
-      expiresIn: "15m",
-    }
-  );
-};
+  createAccessToken(customer: PayloadCustomer, options: TokenOptions) {
+    // Default token expiration: 24 hours
+    const expiresIn = options.expiresIn || "24h";
 
-export const createRefreshToken = (userId: InputCustomerAuth) => {
-  return sign(
-    {
-      user: userId,
+    const payload = {
+      customer: customer.numberPhone,
+      // Add any other user-related data to the payload as needed
+    };
 
-    },
-    process.env.REFRESH_TOKEN_SECRET!,
-    {
-      expiresIn: "7d",
-    }
-  );
-};
+    const accessToken = jwt.sign(payload, this.JWT_SECRET, { expiresIn });
+
+    return accessToken;
+  }
+
+  createRefreshToken(customer: PayloadCustomer, options: TokenOptions) {
+    // Default token expiration: 30 days
+    const expiresIn = options.expiresIn || "30d";
+
+    const payload = {
+      customer: customer.numberPhone,
+      // Add any other user-related data to the payload as needed
+    };
+
+    const refreshToken = jwt.sign(payload, this.REFRESH_TOKEN_SECRET, {
+      expiresIn,
+    });
+
+    return refreshToken;
+  }
+}
