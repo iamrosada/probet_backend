58d143a2c8cc3f041c98dd7c25caed2028aa82ce
iamrosada <luisrosada2018@gmail.com>
2023-10-07 16:02:00 +0300

    feat: deleted some config

--- authentication/src/domain/admin/admin.repository.ts
+++
@@ -1,8 +0,0 @@
-import { AdminEntity, ForgotPasswordResponse } from "./admin.entity";
-
-export interface AdminRepository {
-  findAdminById(uuid: string): Promise<AdminEntity | null>;
-  registerAdmin(Admin: AdminEntity): Promise<AdminEntity | null>;
-  listAdmin(): Promise<AdminEntity[] | null>;
-  forgotPassword(uuid: string): Promise<ForgotPasswordResponse>;
-}
