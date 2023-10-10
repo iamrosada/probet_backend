165f0662af904fc4e3ca6338020a42693b7b45eb
iamrosada <luisrosada2018@gmail.com>
2023-10-10 22:44:18 +0300

    feat: created the main file with sum and mod

--- apps/games-service/cmd/main.go
+++ apps/games-service/cmd/main.go
@@ -38,36 +38,14 @@ func main() {
 	v1 := router.Group(basePath)
 	{
 		v1.POST("/game", gameHandler.CreateGame)
+		v1.GET("/game", gameHandler.GetGameByID)
+		v1.DELETE("/games/delete", gameHandler.DeleteGameByID)
+		v1.GET("/games/categories", gameHandler.GetGamesByCategory)
+		v1.GET("/games", gameHandler.ListAllGames) // New endpoint to list all games
+
+		// New endpoint to get games by category
 
 	}
-	// createUC := usecase.NewGameCreateUseCase(v)
-	// getUC := usecase.NewGameGetUseCase(v)
 
-	// Create a handler instance
-	// gameHandler := http.NewGameHandler(*createUC, *getUC)
-
-	// dbPath := "./db/main.db"
-	// db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
-
-	// if err != nil {
-	// 	panic("failed to connect database")
-
-	// }
-
-	// //Migrate the schemas
-	// err = db.AutoMigrate(&entity.Game{})
-
-	// if err != nil {
-	// 	//panic(err)
-	// 	fmt.Println(err)
-	// 	// logger.Errorf("Config Initialization error: %v", err)
-	// 	return
-	// }
-	// //Initialize Route
-	// router.Initialize()
-
-	// initializeRoutes(router)
-
-	// //Start running the API
 	router.Run(":8080")
 }
